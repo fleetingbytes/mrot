c3dbed16733e93095aa0b14132c99e24374c8741
fleetingbytes <7075397+fleetingbytes@users.noreply.github.com>
2025-02-12 16:14:14 +0100

    feat: introduced tracing

--- src/bin/mrot.rs
+++ src/bin/mrot.rs
@@ -5,14 +5,32 @@
 
 use color_eyre::eyre::Report;
 use mrot::error::Error;
+use tracing::{error, trace};
+use tracing_subscriber::{fmt, prelude::*, EnvFilter};
+
+fn init_tracing() -> () {
+    tracing_subscriber::registry()
+        .with(fmt::layer())
+        .with(EnvFilter::from_env("MROT_LOG_LEVEL"))
+        .init();
+}
 
 fn main() -> Result<(), Report> {
+    init_tracing();
+    trace!("Tracing initialized");
     match mrot::cli::run() {
-        Ok(_) => Ok(()),
-        Err(e) if matches!(e, Error::TimeSpanNotSupported) => {
-            eprintln!("{}", e);
+        Ok(_) => {
+            trace!("cli::run ran without errors");
             Ok(())
         }
-        Err(e) => Err(Report::new(e)),
+        Err(e) if matches!(e, Error::TimeSpanNotSupported) => {
+            trace!("cli::run ran with Error::TimeSpanNotSupported");
+            error!("{}", e);
+            Ok(())
+        }
+        Err(e) => {
+            trace!("cli::run ran with an error");
+            Err(Report::new(e))
+        }
     }
 }
