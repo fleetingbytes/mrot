7e81ba4bae9f2a070402f54685ef5d3905599c39
fleetingbytes <7075397+fleetingbytes@users.noreply.github.com>
2025-03-12 03:57:14 +0100

    refactor: remove color-eyre dependency and unnecessary error types

--- crates/libmrot/src/error.rs
+++ crates/libmrot/src/error.rs
@@ -19,15 +19,11 @@ pub enum Error {
     Sqlite(SqliteError),
     /// wraps [std::fmt::Error]
     Fmt(fmt::Error),
-    /// when something could not be stored in the storage
-    Storage,
     /// wraps [two_timer::TimeError]
     TwoTimer(TimeError),
     /// when a [chrono::NaiveDate] cannot be converted to [chrono::NaiveDateTime]
     TimeNotSupported,
-    /// if the user input a time span instead of time
-    TimeSpanNotSupported,
-    /// when OsString does not contain valid Unicode
+    /// when a path contains an invalid Unicode character
     InvalidUnicode(OsString),
     /// when [directories::ProjectDirs] is not found
     NoDirectory(String),
@@ -35,8 +31,6 @@ pub enum Error {
     NoParentDirectory,
     /// Timestamp cannot be converted into [chrono::DateTime]
     InvalidTimestamp(i64),
-    /// A date which is not an explicit range spans a time period longer than one day
-    DateSpansMoreThanOneDay,
 }
 
 impl fmt::Display for Error {
@@ -46,10 +40,8 @@ impl fmt::Display for Error {
             Error::Confy(confy_error) => fmt::Display::fmt(confy_error, f),
             Error::Sqlite(sqlite_error) => fmt::Display::fmt(sqlite_error, f),
             Error::Fmt(fmt_error) => fmt::Display::fmt(fmt_error, f),
-            Error::Storage => fmt::Display::fmt("cannot store values", f),
             Error::TwoTimer(time_error) => fmt::Display::fmt(time_error, f),
             Error::TimeNotSupported => fmt::Display::fmt("such time is not supported", f),
-            Error::TimeSpanNotSupported => fmt::Display::fmt("time spans are not supported", f),
             Error::InvalidUnicode(os_string) => {
                 fmt::Display::fmt(os_string.to_string_lossy().into_owned().as_str(), f)
             }
@@ -58,10 +50,6 @@ impl fmt::Display for Error {
             }
             Error::NoParentDirectory => fmt::Display::fmt("cannot find parent directory", f),
             Error::InvalidTimestamp(i) => fmt::Display::fmt(&format!("invalid timestamp {}", i), f),
-            Error::DateSpansMoreThanOneDay => fmt::Display::fmt(
-                "a date which is not an explicit range spans a time period longer than one day",
-                f,
-            ),
         }
     }
 }
@@ -73,15 +61,12 @@ impl std::error::Error for Error {
             Error::Confy(ref confy_error) => Some(confy_error),
             Error::Sqlite(ref sqlite_error) => Some(sqlite_error),
             Error::Fmt(ref fmt_error) => Some(fmt_error),
-            Error::Storage => None,
             Error::TwoTimer(ref time_error) => Some(time_error),
             Error::TimeNotSupported => None,
-            Error::TimeSpanNotSupported => None,
             Error::InvalidUnicode(_) => None,
             Error::NoDirectory(_) => None,
             Error::NoParentDirectory => None,
             Error::InvalidTimestamp(_) => None,
-            Error::DateSpansMoreThanOneDay => None,
         }
     }
 }
--- crates/mrot/src/main.rs
+++ crates/mrot/src/main.rs
@@ -2,10 +2,8 @@
 //!
 //!
 
-use color_eyre::eyre::Report;
 use directories::ProjectDirs;
-use libmrot::Error;
-use tracing::error;
+use libmrot::{Error, Result};
 use tracing_appender::non_blocking;
 use tracing_subscriber::{filter::EnvFilter, fmt, fmt::format::FmtSpan, prelude::*};
 
@@ -13,7 +11,7 @@ const LOG_FILE: &str = "trace.log";
 const PKG_NAME: &str = env!("CARGO_PKG_NAME");
 const LOG_LEVEL_ENV_VAR: &str = concat!(env!("PKG_NAME_UPPERCASE"), "_LOG_LEVEL");
 
-fn init_tracing() -> Result<Vec<impl Drop>, Error> {
+fn init_tracing() -> Result<Vec<impl Drop>> {
     let (non_blocking_stderr, stderr_guard) = non_blocking(std::io::stderr());
     let stderr_log_layer = fmt::layer()
         .with_span_events(FmtSpan::NEW | FmtSpan::CLOSE)
@@ -22,7 +20,7 @@ fn init_tracing() -> Result<Vec<impl Drop>, Error> {
         .pretty();
 
     let dirs =
-        ProjectDirs::from("", "", PKG_NAME).ok_or(Error::NoDirectory("ProjectDirs".to_string()))?;
+        ProjectDirs::from("", "", PKG_NAME).ok_or(Error::NoDirectory("directories::ProjectDirs: no valid home directory path could be retrieved from the operating system".to_string()))?;
     let log_file = dirs.data_dir().join(LOG_FILE);
     let log_file_writer = std::fs::File::create(log_file)?;
     let (non_blocking_file, file_guard) = non_blocking(log_file_writer);
@@ -38,15 +36,7 @@ fn init_tracing() -> Result<Vec<impl Drop>, Error> {
     Ok(vec![stderr_guard, file_guard])
 }
 
-fn main() -> Result<(), Report> {
-    color_eyre::install()?;
+fn main() -> Result<()> {
     let _guards = init_tracing()?;
-    match mrot_cli::run() {
-        Ok(_) => Ok(()),
-        Err(e) if matches!(e, Error::TimeSpanNotSupported) => {
-            error!("{}", e);
-            Err(Report::new(e))
-        }
-        Err(e) => Err(Report::new(e)),
-    }
+    mrot_cli::run()
 }
