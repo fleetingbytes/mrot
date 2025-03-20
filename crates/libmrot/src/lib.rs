32c06d1db75be69cb2061eeb47340a76ded226a6
fleetingbytes <7075397+fleetingbytes@users.noreply.github.com>
2025-03-20 06:22:38 +0100

    refactor: change storage.what signature

--- crates/libmrot/src/lib.rs
+++ crates/libmrot/src/lib.rs
@@ -9,6 +9,8 @@ mod error;
 mod meal_record;
 mod storage;
 
+use crate::convert::convert_date_to_timestamp;
+use chrono::NaiveDate;
 pub use convert::parse_date;
 pub use error::Error;
 pub use meal_record::MealRecord;
@@ -16,3 +18,80 @@ pub use storage::Storage;
 
 /// Type alias for results with libmrot's [Error].
 pub type Result<T> = std::result::Result<T, Error>;
+
+/// Holds data related to the look-ahead period in which to search for meals which the user
+/// explicitly planned so that they can be excluded from meal suggestions.
+/// See section [Getting Meal Suggestions](https://github.com/fleetingbytes/mrot/#getting-meal-suggestions) in the mrot readme.
+#[derive(Debug, Clone)]
+pub struct LookAhead {
+    first_day_timestamp: i64,
+    last_day_timestamp: i64,
+    first_date: NaiveDate,
+    last_date: NaiveDate,
+}
+
+impl LookAhead {
+    /// Construct a new `LookAhead`. If *date* is the `Some` variant, it should contain a parsable date expression
+    /// (cf. [parse_date]).
+    ///
+    /// Examples of successfully constructed LookAheads:
+    /// ```
+    /// use libmrot::LookAhead;
+    /// use chrono::Days;
+    ///
+    /// let no_look_ahead: Option<LookAhead> = LookAhead::new(None).unwrap();
+    /// assert!(no_look_ahead.is_none());
+    ///
+    /// let one_day_look_ahead: Option<LookAhead> = LookAhead::new(Some("tomorrow".to_string())).unwrap();
+    /// assert!(one_day_look_ahead.is_some_and(|la| la.first_date() == la.last_date()));
+    ///
+    /// let multiple_day_look_ahead: Option<LookAhead> = LookAhead::new(Some("from tomorrow through 11 days after tomorrow".to_string())).unwrap();
+    /// assert!(multiple_day_look_ahead.is_some_and(|la| la.first_date().checked_add_days(Days::new(11)).unwrap() == la.last_date()));
+    /// ```
+    ///
+    /// Error:
+    /// ```
+    /// use libmrot::{Error, LookAhead};
+    ///
+    /// let unparsable_date = LookAhead::new(Some(String::new())).unwrap_err();
+    /// assert!(matches!(unparsable_date, Error::TwoTimer(_)));
+    /// ```
+    pub fn new(date: Option<String>) -> Result<Option<Self>> {
+        match date {
+            None => Ok(None),
+            Some(date_string) => {
+                let dates = parse_date(&date_string)?;
+                let first_date = dates.iter().next().unwrap();
+                let last_date = dates.iter().last().unwrap();
+                let first_day_timestamp = convert_date_to_timestamp(first_date)?;
+                let last_day_timestamp = convert_date_to_timestamp(last_date)?;
+                Ok(Some(Self {
+                    first_day_timestamp,
+                    last_day_timestamp,
+                    first_date: *first_date,
+                    last_date: *last_date,
+                }))
+            }
+        }
+    }
+
+    /// Returns the [`NaiveDate`] of the first day of the look-ahead period.
+    pub fn first_date(&self) -> NaiveDate {
+        self.first_date
+    }
+
+    /// Returns the timestamp of the first day of the look-ahead period.
+    pub fn first_day_timestamp(&self) -> i64 {
+        self.first_day_timestamp
+    }
+
+    /// Return the [`NaiveDate`] of the last day of the look-ahead period.
+    pub fn last_date(&self) -> NaiveDate {
+        self.last_date
+    }
+
+    /// Returns the timestamp of the last day of the look-ahead period.
+    pub fn last_day_timestamp(&self) -> i64 {
+        self.last_day_timestamp
+    }
+}
